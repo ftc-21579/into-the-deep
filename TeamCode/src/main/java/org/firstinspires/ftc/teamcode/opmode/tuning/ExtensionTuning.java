4f0c96b0af17912f896bcf7efe12ad7f3c3e836a
astr0clad <colin.montigel@gmail.com>
2024-09-08 01:25:46 -0700

    PID Tuning OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/ExtensionTuning.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.opmode.tuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.checkerframework.checker.initialization.qual.Initialized;
+import org.firstinspires.ftc.teamcode.common.Bot;
+import org.firstinspires.ftc.teamcode.common.commandbase.command.extension.SetExtensionElevationCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.command.extension.SetExtensionPositionCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.subsystem.Extension;
+
+@TeleOp(name = "Extension Tuning", group = "Tuning")
+public class ExtensionTuning extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        telemetry = telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Bot bot = new Bot(telemetry, hardwareMap);
+        Extension extension = bot.getExtension();
+
+        GamepadEx operator = new GamepadEx(gamepad1);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            CommandScheduler s = CommandScheduler.getInstance();
+
+            operator.readButtons();
+
+            telemetry.addLine("Extension Tuning - Tune values in Dash");
+            telemetry.addLine("192.168.1.43:8080/dash");
+
+            double targetLength = 0.0;
+
+            if (operator.wasJustPressed(GamepadKeys.Button.A)) {
+                new SetExtensionPositionCommand(extension, 48.0).schedule();
+                telemetry.addData("Extension Target Position", 48.0);
+                targetLength = 48.0;
+            } else if (operator.wasJustPressed(GamepadKeys.Button.Y)) {
+                new SetExtensionPositionCommand(extension, 12.0).schedule();
+                telemetry.addData("Extension Target Position", 12.0);
+                targetLength = 12.0;
+            }
+
+            telemetry.addData("Extension Current Position", (extension.getExtensionMotorPosition() * 0.2495));
+            telemetry.addData("Extension Position Error", targetLength - (extension.getExtensionMotorPosition() * 0.2495));
+
+
+
+            double targetElevation = 0.0;
+
+            if (operator.wasJustPressed(GamepadKeys.Button.X)) {
+                new SetExtensionElevationCommand(extension, Math.PI).schedule();
+                telemetry.addData("Extension Target Elevation", Math.PI);
+                targetElevation = Math.PI;
+            } else if (operator.wasJustPressed(GamepadKeys.Button.B)) {
+                new SetExtensionElevationCommand(extension, (Math.PI / 2)).schedule();
+                telemetry.addData("Extension Target Elevation", Math.PI / 2);
+                targetElevation = Math.PI / 2;
+            }
+
+            // targetTicks = (int) ((elevationRad * 128.3) / (2 * Math.PI)) reversed
+            telemetry.addData("Extension Current Elevation", (extension.getElevationMotorPosition() * (2 * Math.PI) / 128.3));
+            telemetry.addData("Extension Elevation Error", targetElevation - (extension.getElevationMotorPosition() * (2 * Math.PI) / 128.3));
+
+            telemetry.update();
+            s.run();
+        }
+    }
+
+}
