46bcb5402a02c6fe64627a5eb18b5727062ff837
Astr0clad <67985645+Astr0clad@users.noreply.github.com>
2024-10-16 10:18:19 -0700

    Revert "Rewrite code to fit the FTCLib CommandOpMode Paradigm"
    This reverts commit 8d2366f1b17c249e6da797366dfe625a989039f3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOp.java
@@ -1,35 +1,4 @@
 package org.firstinspires.ftc.teamcode.opmode.teleop;
 
-import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.mineinjava.quail.util.geometry.Vec2d;
-
-import org.firstinspires.ftc.teamcode.common.Bot;
-import org.firstinspires.ftc.teamcode.common.commandbase.command.drive.TeleOpDriveCommand;
-import org.firstinspires.ftc.teamcode.common.commandbase.subsystem.MecanumDrivetrain;
-
-public class TeleOp extends CommandOpMode {
-
-    private Bot bot;
-    private MecanumDrivetrain drivetrain;
-
-    private TeleOpDriveCommand driveCommand;
-
-    private GamepadEx driverGamepad, operatorGamepad;
-
-    @Override
-    public void initialize() {
-
-        driverGamepad = new GamepadEx(gamepad1);
-
-        bot = new Bot(telemetry, hardwareMap);
-        drivetrain = bot.getDrivetrain();
-
-        driveCommand = new TeleOpDriveCommand(drivetrain,
-                new Vec2d(driverGamepad.getLeftX(), driverGamepad.getLeftY()),
-                driverGamepad.getRightX(), 0.8);
-
-        register(drivetrain);
-        drivetrain.setDefaultCommand(driveCommand);
-    }
+public class TeleOp {
 }
