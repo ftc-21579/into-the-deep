32b7af30881a75304b1cb44b144e7a036269a790
astr0clad <colin.montigel@gmail.com>
2024-11-03 23:25:24 -0700

    reverse

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/subsystem/Wrist.java
@@ -23,6 +23,7 @@ public class Wrist extends SubsystemBase {
 
         left = bot.hMap.get(Servo.class, "leftWrist");
         right = bot.hMap.get(Servo.class, "rightWrist");
+        right.setDirection(Servo.Direction.REVERSE);
     }
 
     @Override
