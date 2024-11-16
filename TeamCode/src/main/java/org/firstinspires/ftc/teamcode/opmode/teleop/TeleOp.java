46565dd48be47cd902714ea0fc46608fe11f58b2
astr0clad <colin.montigel@gmail.com>
2024-11-15 21:40:51 -0700

    Scheduling the commands is pretty useful lwk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOp.java
@@ -80,8 +80,8 @@ public class TeleOp extends CommandOpMode {
         register(drivetrain);
         drivetrain.setDefaultCommand(driveCommand);
 
-        new SetPivotAngleCommand(pivot, 10);
-        new SetWristPositionCommand(wrist, new Vec2d(0, 135));
+        new SetPivotAngleCommand(pivot, 10).schedule();
+        new SetWristPositionCommand(wrist, new Vec2d(0, 135)).schedule();
         //endregion
 
         //region Claw
