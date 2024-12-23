711f4dfeae7c5dd3b66bd7e4bbc1894b3c541c08
astr0clad <colin.montigel@gmail.com>
2024-12-22 23:11:07 -0700

    Remove old rr stuff that somehow didnt get removed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/util/PathSupplier.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.common.util;
-
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-
-@FunctionalInterface
-public interface PathSupplier {
-    Action getPath(TrajectoryActionBuilder builder);
-}
